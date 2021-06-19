119ab6fa81542a3b43337db2c1239486625799d9
Kirk Lin <linkirk@163.com>
2021-06-20 00:08:49 +0800

    添加Nacos服务代码

--- kkmall-coupon/src/main/java/name/lkk/kkmall/coupon/KkmallCouponApplication.java
+++ kkmall-coupon/src/main/java/name/lkk/kkmall/coupon/KkmallCouponApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("name.lkk.kkmall.coupon.dao")
 public class KkmallCouponApplication {
 
     public static void main(String[] args) {
